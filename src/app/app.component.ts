b713a75c1bfdf6ac236ee6512934bd544f865fec
Ulises Hernández Calzadillas <134593884+uliseshdzc@users.noreply.github.com>
2024-12-06 16:42:09 +0100

    feat: add fade effects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'personal-website';
 
   navigateToUrl(url: string) {
     return () => window.open(url);
+  }
+
+  ngAfterViewInit() {
+    const options = {
+      threshold: 0.25
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const textLightElements = entry.target.querySelectorAll('.text-light');
+          textLightElements.forEach((el, index) => {
+            setTimeout(() => {
+              el.classList.add('animate');
+            }, index * 500); // Delay each animation by 500ms
+          });
+        }
+      });
+    }, options);
+
+    const elements = document.querySelectorAll('.column-container');
+    elements.forEach(element => {
+      observer.observe(element);
+    });
   }
 }
