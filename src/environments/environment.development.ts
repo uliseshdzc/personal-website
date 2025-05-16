0e8c1fd1dafd3cbedb1b4ac0655dff9c68c5f3f0
Ulises Hernández Calzadillas <ulises.hernandez.calzadillas@gmail.com>
2025-05-16 16:37:05 +0200

    build: change questions API url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  questionsUrl: 'http://localhost:8000'
+  questionsUrl: 'https://aws.uliseshernandez.com'
 };
--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  questionsUrl: 'http://ec2-34-238-126-144.compute-1.amazonaws.com'
+  questionsUrl: 'https://aws.uliseshernandez.com'
 };
